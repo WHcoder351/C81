fa5eb74e1c4acd0d047e3f53df6a44ebd271867a
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-02-21 13:11:03 -0500

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,23 @@
+canvas = document.getElementById("Can-Can");
+ ctx = canvas.getContext("2d");
+color = "blue";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown", mouseDown);
+function mouseDown(e){
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y =  e.clientY - canvas.offsetTop;
+    console.log("X = " + mouse_x + " ,Y =  " + mouse_y);
+    circle(mouse_x , mouse_y);  
+}
+function circle(mouse_x , mouse_y)
+{
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+}
